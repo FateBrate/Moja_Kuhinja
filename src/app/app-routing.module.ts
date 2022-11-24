a94d24cca75740e10871b49ed272c40d373e69c0
LAPTOP-620BIS25\drekf <fatih.drek@edu.fit.ba>
2022-11-24 02:03:00 +0100

    Home page

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./stranice/home/home.component";
+import {MenuComponent} from "./stranice/menu/menu.component";
+import {AboutComponent} from "./stranice/about/about.component";
+import {ContactComponent} from "./stranice/contact/contact.component";
+
+const routes:Routes=[
+  {path:'',component:HomeComponent},
+  {path:'menu',component:MenuComponent},
+  {path:'about',component:AboutComponent},
+  {path:'contact',component:ContactComponent}
+];
+
+@NgModule({
+  declarations: [],
+  imports:
+
+    [RouterModule.forRoot(routes)],
+    exports:[RouterModule]
+
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './sharepage/navbar/navbar.component';
+import { FooterComponent } from './sharepage/footer/footer.component';
+import { HomeComponent } from './stranice/home/home.component';
+import { MenuComponent } from './stranice/menu/menu.component';
+import { AboutComponent } from './stranice/about/about.component';
+import { ContactComponent } from './stranice/contact/contact.component';
+import{AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    MenuComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
