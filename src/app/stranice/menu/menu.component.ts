89941f9d4d4fa70bc64e3319f65c14ee3a3e4ae0
LAPTOP-620BIS25\drekf <fatih.drek@edu.fit.ba>
2022-11-29 00:50:33 +0100

    Home zavrsen,Menu zapocet

--- src/app/stranice/menu/menu.component.ts
+++ src/app/stranice/menu/menu.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {DetaljiNarudzbeService} from "../../servisi/detalji-narudzbe.service";
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
+export class MenuComponent implements  OnInit{
+  constructor(private service:DetaljiNarudzbeService) {}
+  hranaPodaci:any;
 
+ngOnInit():void{
+this.hranaPodaci=this.service.JelaPodaci;
+}
 }
